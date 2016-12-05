d07315d77fe0947747f6ed9eab6bae03cc050b21
mudacha <mudachaz@gmail.com>
2016-12-04 18:00:52 -0700

    Updated the basic scene. Orientated the jump in reference to the ground. Added cone to represent tree.

--- skijump.py
+++ skijump.py
@@ -1,9 +1,14 @@
 from visual import *
+
+floor = box(pos=(0.0,-5.0,0.0),size=(100.0,0.2,100),material = materials.rough, color=color.white)
+treeTop = cone(pos=(-500,-100,-100), axis=(9,0,0), radius=4,color=color.green)
+treeTop.pos=(0,5,-40)
+treeTop.rotate(angle=pi/2, axis=(0,0,1), origin=treeTop.pos)
 f = frame()
-semicircle = paths.arc(radius=20, angle2=pi/1.3)
-circ = shapes.rectangle(pos=(-2,3), width=5, height=10)
+semicircle = paths.arc(radius=20,angle1=pi, angle2 = pi * 1.75)
+circ = shapes.rectangle(pos=(0,-10), width=10, height=15)
 ramp = extrusion(frame=f,pos=semicircle, 
           shape=circ, 
-          color=color.yellow)
-f.pos = (-2,3,0)
-f.rotate(angle=pi/4, axis=(0,0,1), origin=f.pos)
+          color=color.white)
+f.pos = (-20,20,10)
+f.rotate(angle=-pi * 1.5, axi2s=(0,0,1), origin=f.pos)
